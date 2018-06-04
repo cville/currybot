39ccbe9a0c4a3074d699edbc0260d674ab3ebbe4
Nick Hamblet <nick.hamblet@gmail.com>
2018-06-04 19:30:56 -0400

    First commit

---
+++ src/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE CPP #-}
+
+module Main where
+
+import Web.Slack
+import Web.Slack.Message
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative
+#endif
+
+myConfig :: String -> SlackConfig
+myConfig apiToken = SlackConfig
+         { _slackApiToken = apiToken -- Specify your API token here
+         }
+
+echoBot :: SlackBot ()
+echoBot (Message cid _ msg _ (Just (SChannelTopic _)) _) = sendMessage cid ("New channel topic: "++msg)
+echoBot _ = return ()
+
+main :: IO ()
+main = do
+  apiToken <- fromMaybe (error "SLACK_API_TOKEN not set")
+               <$> lookupEnv "SLACK_API_TOKEN"
+  runBot (myConfig apiToken) echoBot ()
+
